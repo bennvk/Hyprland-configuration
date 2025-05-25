20e266abe2982011723856000eacbf2b8337cea7
bennvk <bne.ppn@gmail.com>
2025-05-25 20:52:43 +0200

    Update waybar

--- .config/waybar/musicplayer.sh
+++ .config/waybar/musicplayer.sh
@@ -5,15 +5,12 @@ artist=$(playerctl metadata artist 2>/dev/null)
 title=$(playerctl metadata title 2>/dev/null)
 
 if [ "$status" = "Playing" ]; then
-    icon=""
-elif [ "$status" = "Paused" ]; then
-    icon=""
+    icon=""
 else
-    echo '{"text": "", "tooltip": "Aucune lecture en cours"}'
     exit 0
 fi
 
-text="$icon $artist - $title"
+text="$icon   $artist - $title"
 
 echo "{\"text\": \"$text\", \"tooltip\": \"$artist - $title\"}"
 
