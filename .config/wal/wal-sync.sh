1bf1a66d651aebb71f71d513f24530c918817fbe
bennvk <bne.ppn@gmail.com>
2025-06-05 23:44:29 +0200

    Update wal-sync.sh

--- .config/wal/wal-sync.sh
+++ .config/wal/wal-sync.sh
@@ -11,17 +11,6 @@ COLORS_JSON="$HOME/.cache/wal/colors.json"
 
 wal -i "$WALLPAPER_PATH" -n -q
 
-##### Fond d'écran avec SWWW #####
-
-monitors=$(hyprctl monitors -j | jq -r '.[].name' | paste -sd "," -)
-
-swww img "$WALLPAPER_PATH" \
-  --outputs "$monitors" \
-  --transition-type grow \
-  --transition-pos 0.5,0.5 \
-  --transition-duration 1 \
-  --transition-fps 60
-
 ##### Kitty #####
 
 cat > ~/.config/kitty/colors.conf <<EOF
