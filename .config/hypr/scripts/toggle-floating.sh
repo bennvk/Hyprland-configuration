bc001a3b8adec53f5da1565d5acb94dad55fec09
bennvk <bne.ppn@gmail.com>
2025-07-26 06:46:10 +0200

    Update hyprland conf (toggle-floating script)

--- .config/hypr/scripts/toggle-floating.sh
+++ .config/hypr/scripts/toggle-floating.sh
@@ -1,14 +1,5 @@
 #!/bin/bash
 
-c=$(hyprctl cursorpos -j)
-x=$(jq -r .x <<<"$c")
-y=$(jq -r .y <<<"$c")
-
-win=$(hyprctl clients -j | jq -r --argjson x $x --argjson y $y '
-  [.[] | select(.at[0]<=$x and $x<(.at[0]+.size[0]) and .at[1]<=$y and $y<(.at[1]+.size[1]))][-1].address')
-
-[[ -z $win ]] && exit
-
-hyprctl dispatch focuswindow address:$win
-hyprctl dispatch togglefloating address:$win
+hyprctl dispatch togglefloating
 hyprctl dispatch resizeactive exact 1500 900
+hyprctl dispatch centerwindow
