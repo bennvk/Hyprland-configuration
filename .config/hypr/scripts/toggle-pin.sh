8f9d888ec0e0199adbf429b9fa7016c88b9c1f5d
bennvk <bne.ppn@gmail.com>
2025-07-27 11:45:22 +0200

    Add toggle-pin script

---
+++ .config/hypr/scripts/toggle-pin.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+WHITE="0xffffffff"
+TRANSPARENT="0x00000000"
+
+window_info=$(hyprctl activewindow -j)
+
+is_floating=$(echo "$window_info" | jq -r '.floating')
+
+if [[ "$is_floating" != "true" ]]; then
+    hyprctl dispatch togglefloating activewindow
+    hyprctl dispatch resizeactive exact 1500 900
+    hyprctl dispatch centerwindow activewindow
+fi
+
+is_pinned=$(echo "$window_info" | jq -r '.pinned')
+
+if [[ "$is_pinned" == "true" ]]; then
+    hyprctl dispatch pin activewindow
+    hyprctl dispatch setprop activewindow activebordercolor "$TRANSPARENT"
+else
+    hyprctl dispatch pin activewindow
+    hyprctl dispatch setprop activewindow activebordercolor "$WHITE"
+fi
+
