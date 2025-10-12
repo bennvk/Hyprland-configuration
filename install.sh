420989088dcdb6b9813bd59aea89c99ebab4407e
benjamin <210059635+bennvk@users.noreply.github.com>
2025-10-12 13:16:30 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
-#!usr/bin/env bash
+#!/usr/bin/env bash
 
 repo="https://github.com/bennvk/Hyprland-configuration.git"
-configdir=$HOME/.config
+configdir="$HOME/.config"
 
 echo ">>> Script d'installation de la configuration <<<"
 
@@ -9,8 +9,8 @@ sleep 1
 
 echo ">>> Installation des paquets et dépendances <<<"
 sudo pacman -S --needed \
-  hyprland swww waybar rofi bash kitty hyprlock nvidia nvidia-utils nvidia-settings lib32-nvidia-utils gamemode gamescope vulkan-icd-loader lib32-vulkan-icd-loader \
-  python-pywal starship ttf-cascadia-code ttf-nerd-fonts-symbols ttf-nerd-fonts-symbols-common papirus-icon-theme fastfetch \
+  hyprland swww waybar rofi kitty hyprlock nvidia nvidia-utils nvidia-settings lib32-nvidia-utils gamemode gamescope \
+  vulkan-icd-loader lib32-vulkan-icd-loader python-pywal starship ttf-cascadia-code papirus-icon-theme fastfetch \
   neovim zip nautilus galculator grim slurp wl-clipboard brightnessctl ddcutil imv pavucontrol
 
 yay -S --needed python-pywalfox ttf-font-awesome-5
@@ -19,46 +19,48 @@ pywalfox install
 sleep 1
 
 echo ">>> Copie de la configuration <<<"
-cp Hyprland-Configuration/.bashrc $HOME/.bashrc
-cp Hyprland-Configuration/.bash_profile $HOME/.bash_profile
-cp Hyprland-Configuration/.config/fastfetch $configdir/fastfetch
-cp Hyprland-Configuration/.config/help $configdir/help
-cp Hyprland-Configuration/.config/hypr $configdir/hypr
-cp Hyprland-Configuration/.config/hyprlock $configdir/hyprlock
-cp Hyprland-Configuration/.config/kitty $configdir/kitty
-cp Hyprland-Configuration/.config/nvim $configdir/nvim
-cp Hyprland-Configuration/.config/rofi $configdir/rofi
-cp Hyprland-Configuration/.config/starship $configdir/starship
-cp Hyprland-Configuration/.config/wal $configdir/wal
-cp Hyprland-Configuration/.config/fastfetch $configdir/waybar
+cp -r .bashrc "$HOME/.bashrc"
+cp -r .bash_profile "$HOME/.bash_profile"
+cp -r .config/fastfetch "$configdir/fastfetch"
+cp -r .config/help "$configdir/help"
+cp -r .config/hypr "$configdir/hypr"
+cp -r .config/hyprlock "$configdir/hyprlock"
+cp -r .config/kitty "$configdir/kitty"
+cp -r .config/nvim "$configdir/nvim"
+cp -r .config/rofi "$configdir/rofi"
+cp -r .config/starship "$configdir/starship"
+cp -r .config/wal "$configdir/wal"
+cp -r .config/waybar "$configdir/waybar"
 
 sleep 1
 
-read -p "Importer les fonds d'écrans ? [O/n]" choix_wp
-
+read -p "Importer les fonds d'écrans ? [O/n] " choix_wp
 case $choix_wp in
-    [Oo]) 
-        sudo cp Hyprland-Configuration/Images $HOME/Images
+    [Oo]|"")
+        cp -r Hyprland-Configuration/Images "$HOME/Images"
         ;;
-    [Nn]) 
+    [Nn])
         ;;
-    *) echo "Choix non valide, veuillez réessayer"
-       exit 1
-       ;;
+    *)
+        echo "Choix non valide, veuillez réessayer."
+        exit 1
+        ;;
 esac
 
 sleep 1
 
-read -p "Importer la configuration de clavier qwerty ? [O/n]" choix_kb
-
+read -p "Importer la configuration de clavier qwerty ? [O/n] " choix_kb
 case $choix_kb in
-    [Oo]) 
-        sudo cp Hyprland-Configuration/etc/keyd /etc/keyd
-        sudo pacman -S keyd
+    [Oo]|"")
+        sudo pacman -S --needed keyd
+        sudo cp -r Hyprland-Configuration/etc/keyd /etc/keyd
         ;;
-    [Nn]) 
+    [Nn])
         ;;
-    *) echo "Choix non valide, veuillez réessayer"
-       exit 1
-       ;;
+    *)
+        echo "Choix non valide, veuillez réessayer."
+        exit 1
+        ;;
 esac
+
+echo ">>> Installation terminée avec succès 🎉"
