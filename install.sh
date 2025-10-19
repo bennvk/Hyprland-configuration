4394138a58aef424639e24fd5659c47160a9e3fe
benjamin <210059635+bennvk@users.noreply.github.com>
2025-10-19 17:34:53 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
-repo="https://github.com/bennvk/Hyprland-configuration.git"
 configdir="$HOME/.config"
 
 echo ">>> Script d'installation de la configuration <<<"
@@ -11,7 +10,7 @@ echo ">>> Installation des paquets et dépendances <<<"
 sudo pacman -S --needed \
 hyprland swww waybar rofi kitty hyprlock nvidia nvidia-utils nvidia-settings lib32-nvidia-utils vulkan vulkan-tools vulkan-icd-loader lib32-vulkan-icd-loader gamemode python-pywal starship ttf-cascadia-code papirus-icon-theme fastfetch neovim zip nautilus galculator grim slurp wl-clipboard brightnessctl ddcutil imv pavucontrol
 
-yay -S --needed python-pywalfox ttf-font-awesome-5 vscodium-bin
+yay -S --needed python-pywalfox ttf-font-awesome-5
 pywalfox install
 
 sleep 1
@@ -35,7 +34,7 @@ sleep 1
 read -p "Importer les fonds d'écrans ? [O/n] " choix_wp
 case $choix_wp in
     [Oo]|"")
-        cp -r Hyprland-Configuration/Images "$HOME/Images"
+        cp -r Images "$HOME/Images"
         ;;
     [Nn])
         ;;
@@ -51,7 +50,7 @@ read -p "Importer la configuration de clavier qwerty ? [O/n] " choix_kb
 case $choix_kb in
     [Oo]|"")
         sudo pacman -S --needed keyd
-        sudo cp -r Hyprland-Configuration/etc/keyd /etc/keyd
+        sudo cp -r etc/keyd /etc/keyd
         ;;
     [Nn])
         ;;
