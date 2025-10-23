32874298c928d38f38d2a271e318c33ba1b4879d
benjamin <210059635+bennvk@users.noreply.github.com>
2025-10-23 13:08:56 +0200

    Add package installation script for pacman and AUR
    This script installs a list of essential packages using pacman and AUR.

---
+++ packages.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+echo ">>> Installation des paquets pacman <<<"
+
+sudo pacman -S --needed --noconfirm \
+  hyprland \
+  swww \
+  waybar \
+  rofi \
+  kitty \
+  hyprlock \
+  nvidia \
+  nvidia-utils \
+  nvidia-settings \
+  lib32-nvidia-utils \
+  vulkan \
+  vulkan-tools \
+  vulkan-icd-loader \
+  lib32-vulkan-icd-loader \
+  gamemode \
+  lib32-gamemode \
+  python-pywal \
+  starship \
+  ttf-cascadia-code \
+  papirus-icon-theme \
+  fastfetch \
+  neovim \
+  zip \
+  nautilus \
+  galculator \
+  grim \
+  slurp \
+  wl-clipboard \
+  brightnessctl \
+  ddcutil \
+  imv \
+  pavucontrol
+
+echo ">>> Installation des paquets AUR <<<"
+
+yay -S --needed --noconfirm \
+  librewolf-bin \
+  python-pywalfox-librewolf \
+  ttf-font-awesome-5
