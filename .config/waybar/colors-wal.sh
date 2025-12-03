c04fe70b8b988e8f42bbb4898903772b09ce408e
bennvk <benppn@proton.me>
2025-12-03 16:52:59 +0100

    Update waybar style (gradient)

--- .config/waybar/colors-wal.sh
+++ .config/waybar/colors-wal.sh
@@ -5,28 +5,47 @@ foreground=$(jq -r '.special.foreground' ~/.cache/wal/colors.json)
 color1=$(jq -r '.colors.color1' ~/.cache/wal/colors.json)
 color2=$(jq -r '.colors.color2' ~/.cache/wal/colors.json)
 
+gradient() {
+    hex=$1
+    r=$((16#${hex:1:2}))
+    g=$((16#${hex:3:2}))
+    b=$((16#${hex:5:2}))
+    r=$(( r * 80 / 100 ))
+    g=$(( g * 80 / 100 ))
+    b=$(( b * 80 / 100 ))
+    printf "#%02x%02x%02x\n" $r $g $b
+}
+
+color2_gradient=$(gradient $color2)
+
 cat > ~/.config/waybar/colors-wal.css <<EOF
-#custom-os_button,
 #workspaces,
-#custom-musicplayer,
 #window,
 #custom-taskbar,
-#pulseaudio,
 #custom-cpu,
-#custom-gpuNmemory,
 #network,
-#battery,
-#battery.charging, #battery.plugged,
-#battery.critical:not(.charging),
-#clock,
 #custom-configuration,
-#custom-power,
+#custom-power {
+    background: linear-gradient(90deg, $color2, $color2_gradient);
+}
+
+#custom-os_button,
+#custom-musicplayer,
+#pulseaudio,
+#custom-gpuNmemory,
+#clock {
+    background: linear-gradient(90deg, $color2_gradient, $color2);
+}
+
 #custom-separator,
-#pulseaudio-slider {
-    background-color: $color2;
+#battery,
+#battery.charging,
+#battery.plugged,
+#battery.critical:not(.charging) {
+    background: $color2_gradient;
 }
 
 tooltip {
-    background: $color2;
+    background: linear-gradient(135deg, $color2, $color2_gradient);
 }
 EOF
