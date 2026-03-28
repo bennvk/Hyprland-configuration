06712def73a9a2e36106599596f4a6dc67c48b17
bennvk <benppn@proton.me>
2026-03-28 21:34:43 +0100

    Update awww animation duration

--- .config/rofi/wallpaper-menu/wallpaper-menu.sh
+++ .config/rofi/wallpaper-menu/wallpaper-menu.sh
@@ -44,7 +44,7 @@ if [ -n "$selected" ]; then
     --outputs "$monitors" \
     --transition-type grow \
     --transition-pos 0.25,0.5 \
-    --transition-duration 2.5 \
+    --transition-duration 2 \
     --transition-fps 60
     bash -ic "~/.config/wal/wal-sync.sh '$selected'" >/dev/null 2>&1 &
 fi
