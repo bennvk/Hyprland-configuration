dcde61ab46297e6351ac604d935c8154952cc0c1
bennvk <benppn@proton.me>
2026-04-28 16:48:45 +0200

    Update nvim status line

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -12,6 +12,8 @@ vim.cmd("filetype plugin indent on")
 vim.keymap.set({'n', 'v'}, 'd', '"_d')
 vim.keymap.set('n', 'dd', '"_dd')
 
+vim.opt.statusline = " %f %r %=%l:%c | %p%% "
+
 vim.api.nvim_create_autocmd("VimEnter", {
   callback = function()
     require("themes.pywal").setup()
