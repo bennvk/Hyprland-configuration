76f9b033993c2da33e75e38d0e67310ff3da510e
bennvk <benppn@proton.me>
2026-05-04 21:47:47 +0200

    Update hyprland config name

--- .config/hypr/scripts/toggle-layout.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-layout=$(hyprctl getoption general:layout | awk 'NR==1{print $2}')
-
-if [ "$layout" = "dwindle" ]; then
-    hyprctl keyword general:layout master
-else
-    hyprctl keyword general:layout dwindle
-fi
