89b688213b2514f57793a60cfbf3f2986fe13ddf
bennvk <benppn@proton.me>
2026-05-12 09:26:25 +0200

    Update hyprland binds, fix close bind + add kill bind

--- .config/hypr/modules-lua/binds.lua
+++ .config/hypr/modules-lua/binds.lua
@@ -9,12 +9,13 @@
 --
 
 -- Application
-hl.bind("SUPER + Q",       hl.dsp.exec_cmd("kitty"))
-hl.bind("SUPER + W",       hl.dsp.exec_cmd("kitty yazi"))
-hl.bind("SUPER + E",       hl.dsp.window.kill())
-hl.bind("SUPER + R",       hl.dsp.exec_cmd("rofi -show drun"))
-hl.bind("SUPER + L",       hl.dsp.exec_cmd("bash ~/.config/rofi/power-menu/power-menu.sh"))
-hl.bind("SUPER + SPACE",   hl.dsp.exec_cmd("bash ~/.config/waybar/modules-scripts/toggle-waybar.sh"))
+hl.bind("SUPER + Q", hl.dsp.exec_cmd("kitty"))
+hl.bind("SUPER + W", hl.dsp.exec_cmd("kitty yazi"))
+hl.bind("SUPER + E", hl.dsp.window.close(activewindow))
+hl.bind("SUPER + SHIFT + E", hl.dsp.window.kill())
+hl.bind("SUPER + R", hl.dsp.exec_cmd("rofi -show drun"))
+hl.bind("SUPER + L", hl.dsp.exec_cmd("bash ~/.config/rofi/power-menu/power-menu.sh"))
+hl.bind("SUPER + SPACE", hl.dsp.exec_cmd("bash ~/.config/waybar/modules-scripts/toggle-waybar.sh"))
 
 -- Windows
 hl.bind("SUPER + F", function()
@@ -22,40 +23,40 @@ hl.bind("SUPER + F", function()
     hl.dispatch(hl.dsp.window.resize({ size = { 1500, 900 }, exact = true }))
     hl.dispatch(hl.dsp.window.center())
 end)
-hl.bind("SUPER + SHIFT + F",  hl.dsp.window.fullscreen())
-hl.bind("SUPER + ESCAPE",   hl.dsp.window.set_prop({ prop = "opaque", value = "toggle" }))
+hl.bind("SUPER + SHIFT + F", hl.dsp.window.fullscreen())
+hl.bind("SUPER + ESCAPE", hl.dsp.window.set_prop({ prop = "opaque", value = "toggle" }))
 
 -- Clipboard
 hl.bind("SUPER + SHIFT + C", hl.dsp.exec_cmd("cliphist wipe"))
-hl.bind("SUPER + C",       hl.dsp.exec_cmd("~/.config/rofi/clipboard-menu/clipboard-menu.sh delete"))
-hl.bind("SUPER + V",       hl.dsp.exec_cmd("~/.config/rofi/clipboard-menu/clipboard-menu.sh paste"))
+hl.bind("SUPER + C", hl.dsp.exec_cmd("~/.config/rofi/clipboard-menu/clipboard-menu.sh delete"))
+hl.bind("SUPER + V", hl.dsp.exec_cmd("~/.config/rofi/clipboard-menu/clipboard-menu.sh paste"))
 
 -- Workspaces
 for i = 1, 10 do
     local key = i % 10
-    hl.bind("SUPER + " .. key,             hl.dsp.focus({ workspace = i}))
-    hl.bind("SUPER + SHIFT + " .. key,     hl.dsp.window.move({ workspace = i }))
+    hl.bind("SUPER + " .. key, hl.dsp.focus({ workspace = i}))
+    hl.bind("SUPER + SHIFT + " .. key, hl.dsp.window.move({ workspace = i }))
 end
 
 -- Screenshot
 hl.bind("SUPER + P", hl.dsp.exec_cmd(
     "bash -c 'grim - | tee >(wl-copy) > ~/Images/Screenshots/screenshot_$(date +%s).png'"
 ))
-hl.bind("SUPER + CTRL + P", hl.dsp.exec_cmd(
+hl.bind("SUPER + SHIFT + P", hl.dsp.exec_cmd(
     "bash -c 'coords=$(slurp) && sleep 0.1 && grim -g \"$coords\" - | tee >(wl-copy) > ~/Images/Screenshots/screenshot_$(date +%s).png'"
 ))
 
 -- Brightness
-hl.bind("SUPER + bracketleft",   hl.dsp.exec_cmd("~/.config/hypr/scripts/brightness-control.sh down"))
-hl.bind("SUPER + bracketright",  hl.dsp.exec_cmd("~/.config/hypr/scripts/brightness-control.sh up"))
+hl.bind("SUPER + bracketleft", hl.dsp.exec_cmd("~/.config/hypr/scripts/brightness-control.sh down"))
+hl.bind("SUPER + bracketright", hl.dsp.exec_cmd("~/.config/hypr/scripts/brightness-control.sh up"))
 hl.bind("XF86MonBrightnessDown", hl.dsp.exec_cmd("~/.config/hypr/scripts/brightness-control.sh down"))
-hl.bind("XF86MonBrightnessUp",   hl.dsp.exec_cmd("~/.config/hypr/scripts/brightness-control.sh up"))
+hl.bind("XF86MonBrightnessUp", hl.dsp.exec_cmd("~/.config/hypr/scripts/brightness-control.sh up"))
 
 -- Media
-hl.bind("SUPER + F5",           hl.dsp.exec_cmd("playerctl previous"))
-hl.bind("SUPER + F6",           hl.dsp.exec_cmd("~/.config/hypr/scripts/volume-control.sh down"))
-hl.bind("SUPER + F7",           hl.dsp.exec_cmd("~/.config/hypr/scripts/volume-control.sh up"))
-hl.bind("SUPER + F8",           hl.dsp.exec_cmd("playerctl next"))
+hl.bind("SUPER + F5", hl.dsp.exec_cmd("playerctl previous"))
+hl.bind("SUPER + F6", hl.dsp.exec_cmd("~/.config/hypr/scripts/volume-control.sh down"))
+hl.bind("SUPER + F7", hl.dsp.exec_cmd("~/.config/hypr/scripts/volume-control.sh up"))
+hl.bind("SUPER + F8", hl.dsp.exec_cmd("playerctl next"))
 hl.bind("XF86AudioRaiseVolume", hl.dsp.exec_cmd("~/.config/hypr/scripts/volume-control.sh up"))
 hl.bind("XF86AudioLowerVolume", hl.dsp.exec_cmd("~/.config/hypr/scripts/volume-control.sh down"))
 
