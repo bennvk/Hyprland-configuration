4ef4f95ec99ce97b7b8e7fef79f6df31c8af1073
bennvk <benppn@proton.me>
2026-05-19 11:34:53 +0200

    Update rofi power-menu (delete enable/disable effects

--- .config/rofi/power-menu/power-profiles.sh
+++ .config/rofi/power-menu/power-profiles.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-mode=$(printf "🔴  Performance\n🟡  Equilibré\n🟢  Économie\n💡  Activer les effets\n🔌  Désactiver les effets\n❌  Annuler" | \
+mode=$(printf "🔴  Performance\n🟡  Equilibré\n🟢  Économie\n❌  Annuler" | \
   rofi -dmenu -theme ~/.config/rofi/power-menu/power-menu1.rasi)
 
 case "$mode" in
@@ -14,25 +14,6 @@ case "$mode" in
 
   *Économie*)
     powerprofilesctl set power-saver
-    ;;
-
-  *Activer\ les\ effets*) 
-    hyprctl reload
-    notify-send "Mode de consommation" "Les effets sont activés"
-    ;;
-
-  *Désactiver\ les\ effets*)
-    hyprctl --batch "
-      keyword animations:enabled 0;
-      keyword decoration:blur:enabled 0;
-      keyword decoration:shadow:enabled 0;
-      keyword decoration:rounding 0;
-      keyword decoration:fullscreen_opacity 1;
-      keyword decoration:blur:enabled 0;
-      keyword decoration:dim_inactive 0;
-      keyword general:gaps_in 0;
-      keyword general:gaps_out 0;
-    notify-send "Mode de consommation" "Tous les effets sont désactivés""
     ;;
   *)
     exit 0
