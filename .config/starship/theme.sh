08ed98eff21761ce06e5ff118d014d6243abc44a
bennvk <benppn@proton.me>
2026-05-26 09:59:06 +0200

    Update starship, fix python venv colors

--- .config/starship/theme.sh
+++ .config/starship/theme.sh
@@ -8,6 +8,7 @@ C4=$(jq -r '.colors.color4' < "$colors_file")
 C5=$(jq -r '.colors.color5' < "$colors_file")
 C6=$(jq -r '.colors.color6' < "$colors_file")
 C7=$(jq -r '.colors.color7' < "$colors_file")
+FG=$(jq -r '.special.foreground' < "$colors_file")
 
 cat > "$HOME/.config/starship/starship.toml" <<EOF
 format = "  \$username\$directory\$git_status\$python\$line_break\$character"
@@ -18,8 +19,8 @@ style_root = "bold red"
 show_always = true
 
 [git_status]
-format = '([with ]($style)[\$all_status\$ahead_behind](bold $C3) )'
-style = "bold $C3"
+format = '(with (\$style)[\$all_status\$ahead_behind](bold $C3) )'
+style = "bold $FG"
 ahead = "⇡\${count}"
 behind = "⇣\${count}"
 diverged = "⇕⇡\${ahead_count}⇣\${behind_count}"
@@ -31,7 +32,7 @@ stashed = "󰏗 "
 conflicted = "⚠\${count} "
 
 [python]
-format = "([with]($style)\${virtualenv})"
+format = "(with (\$style)[\${virtualenv}](\$style) )"
 style = "bold $C3"
 
 [directory]
