3b9307caa2503e8a91e7907f4599effc75ef5683
bennvk <benppn@proton.me>
2026-05-26 17:39:11 +0200

    Update neovim, clean up

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,16 +1,32 @@
 vim.opt.number = true
 vim.opt.clipboard = "unnamedplus"
+vim.opt.statusline = "  %f %r %=%l:%c | %p%%  "
+
 vim.o.mouse = "a"
 vim.o.expandtab = true
 vim.o.shiftwidth = 2
 vim.o.tabstop = 2
 vim.o.softtabstop = 2
+
 vim.g.mapleader = " "
+
 vim.cmd("syntax enable")
 vim.cmd("filetype plugin indent on")
+
 vim.keymap.set({'n', 'v'}, 'd', '"_d')
 vim.keymap.set('n', 'dd', '"_dd')
-vim.opt.statusline = " %f %r %=%l:%c | %p%% "
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git", "clone", "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable",
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
 vim.api.nvim_create_autocmd("VimEnter", {
   callback = function()
     require("themes.pywal").setup()
