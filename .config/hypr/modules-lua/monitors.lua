998a5c8633a0380026dd2a2bb3056562375eef7e
bennvk <benppn@proton.me>
2026-06-04 08:39:28 +0200

    Update hyprland monitors config (+ HDMI conf)

--- .config/hypr/modules-lua/monitors.lua
+++ .config/hypr/modules-lua/monitors.lua
@@ -6,8 +6,10 @@
 --  |_| |_| |_|\___/|_| |_|_|\__\___/|_|  |___/
 --   
 
-hl.monitor({ output = "eDP-1", mode = "1920x1080@144", position = "0x0", scale = 1 })
-hl.monitor({ output = "DP-1", mode = "1920x1080@180", position = "1920x0", scale = 1 })
+hl.monitor({ output = "eDP-1", mode = "highres@highrr", position = "0x0", scale = 1 })
+hl.monitor({ output = "DP-1", mode = "highres@highrr", position = "1920x0", scale = 1 })
 hl.monitor({ output = "DP-2", mode = "1920x1080", position = "1920x0", scale = 1 })
 hl.monitor({ output = "DP-3", mode = "1920x1080", position = "3840x0", scale = 1 })
 hl.monitor({ output = "DP-4", mode = "1920x1080", position = "5760x0", scale = 1 })
+
+hl.monitor({ output = "HDMI-A-1", mode = "highres@highrr", scale = 1 , mirror = "eDP-1"})
