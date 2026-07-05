9e6d7b171a0d22156efcca786ee1be7562f4d981
bennvk <benppn@proton.me>
2026-07-05 19:15:25 +0200

    Update hyprland config for localsend window rule

--- .config/hypr/modules/windowsNworkspaces.lua
+++ .config/hypr/modules/windowsNworkspaces.lua
@@ -8,12 +8,12 @@
 
 -- Workspaces
 
-hl.workspace_rule({workspace = "1", monitor = "eDP-1", default = true})
-hl.workspace_rule({workspace = "2", monitor = "eDP-1"})
-hl.workspace_rule({workspace = "3", monitor = "eDP-1"})
-hl.workspace_rule({workspace = "4", monitor = "DP-1", default = true})
-hl.workspace_rule({workspace = "5", monitor = "DP-1"})
-hl.workspace_rule({workspace = "6", monitor = "DP-1"})
+hl.workspace_rule({workspace = "1", monitor = "HDMI-A-1", default = true})
+hl.workspace_rule({workspace = "2", monitor = "HDMI-A-1"})
+hl.workspace_rule({workspace = "3", monitor = "HDMI-A-1"})
+hl.workspace_rule({workspace = "4", monitor = "eDP-1", default = true})
+hl.workspace_rule({workspace = "5", monitor = "eDP-1"})
+hl.workspace_rule({workspace = "6", monitor = "eDP-1"})
 
 -- Windows 
 
